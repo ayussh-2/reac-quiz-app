97c67640196d8efd19ded81f78c3215a471496ce
ayussh-2 <ayushatschool5@gmail.com>
2024-02-15 20:03:52 +0530

    it is live!!!

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASk9JnNurojBu7yyc6hLcQPkVkMIcki38",
+    authDomain: "quiz-react-5527e.firebaseapp.com",
+    projectId: "quiz-react-5527e",
+    storageBucket: "quiz-react-5527e.appspot.com",
+    messagingSenderId: "954195278373",
+    appId: "1:954195278373:web:f2cacf9d8cd40b78525dcc",
+};
+
+const app = initializeApp(firebaseConfig);
